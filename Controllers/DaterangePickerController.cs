885029e5fbbbae9fef651c179f8c461239d010bb
unknown <thittth1807067@fpt.edu.vn>
2019-12-16 21:25:16 +0700

    demo

---
+++ Controllers/DaterangePickerController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace demo1.Controllers
+{
+    public class DaterangePickerController : Controller
+    {
+        // GET: DaterangePicker
+        public ActionResult Index( DateTime? start)
+        {
+
+            return View();
+        }
+    }
+}
